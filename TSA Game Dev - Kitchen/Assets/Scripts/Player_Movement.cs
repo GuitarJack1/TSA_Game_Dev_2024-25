9bd9d5d78fadfccbf806940a45a4d5faacc487bb
GuitarJack1 <tigerjack07@icloud.com>
2024-10-23 20:21:23 -0500

    Sample scene with player movement

---
+++ TSA Game Dev - Kitchen/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Player_Controls playerControls;
+
+    [SerializeField]
+    private float maxSpeed;
+    [SerializeField]
+    private float speedForce;
+
+    [SerializeField]
+    private float rotationSpeed;
+
+    public enum Controls
+    {
+        WASD,
+        Arrow,
+        TFGH,
+        IJKL
+    }
+
+    public Controls controls;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        playerControls = new Player_Controls();
+        playerControls.Players.Enable();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 movementInput;
+
+        switch (controls)
+        {
+            case Controls.WASD:
+                movementInput = playerControls.Players.WASD.ReadValue<Vector2>();
+                break;
+            case Controls.Arrow:
+                movementInput = playerControls.Players.Arrows.ReadValue<Vector2>();
+                break;
+            case Controls.TFGH:
+                movementInput = playerControls.Players.TFGH.ReadValue<Vector2>();
+                break;
+            case Controls.IJKL:
+                movementInput = playerControls.Players.IJKL.ReadValue<Vector2>();
+                break;
+            default:
+                movementInput = new Vector2(0, 0);
+                break;
+        }
+
+        rb.AddForce(new Vector3(movementInput.x, 0, movementInput.y) * speedForce);
+
+        Vector3 movementInput3D = new Vector3(movementInput.x, 0, movementInput.y);
+        if (movementInput3D != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementInput3D, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed);
+        }
+
+        speedLimit();
+    }
+
+    void speedLimit()
+    {
+        Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+
+        if (flatVel.magnitude > maxSpeed)
+        {
+            flatVel = flatVel.normalized * maxSpeed;
+            rb.linearVelocity = new Vector3(flatVel.x, rb.linearVelocity.y, flatVel.z);
+        }
+
+    }
+}
+
